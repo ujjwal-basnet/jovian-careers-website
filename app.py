19538645fa925d690448004d101ef916a2905429
ujjwal <124008221+ujjwal-basnet@users.noreply.github.com>
2023-05-14 00:05:59 +0545

    render template

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask 
-app = Flask(__name__)
-@app.route("/")#it means home page
-def hellow_word():
-    return "hellow world "
+from flask import Flask, render_template
 
-if __name__ ==  "__main__":
-    app.run()
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
     
+
